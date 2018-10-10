7cea8e0d3173965c8aa5e41e99cdabb8499c1d3b
mgoforth6 <mgoforth@iu.edu>
2018-10-10 01:06:48 -0400

    get

--- public/api/comment.php
+++ public/api/comment.php
@@ -7,10 +7,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
+$id = intval($_GET['id'] ?? 0);
+
+if ($id <1) {
+  throw new Exception('Invalid Task ID');
+}
+
 $commentArr = Comment::getAllCommentById();
 
-$json = json_encode($commentArr);
+$json = json_encode($commentArr, JSON_PRETTY_PRINT);
 
+header('Content-Type: application/json');
 echo $json;
 // database
 // convert to Json
